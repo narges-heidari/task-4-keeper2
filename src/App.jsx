e4e4ff834023f49175c1b5b19512e0b3de22d191
narges-heidari <wintergirl635@gmail.com>
2024-10-22 10:13:20 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import NotesList from './component/NotesList';
+
+const App = () => {
+  return (
+    <div>
+      <NotesList />
+    </div>
+  );
+};
+
+export default App
---
+++ src/component/NotesList.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './Note.module.css'
+
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  }
+];
+
+const NotesList = () => {
+  return (
+    <div className={styles.notesContainer}>
+      {notes.map(note => (
+        <div className={styles.noteItem} key={note.key}>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NotesList;
